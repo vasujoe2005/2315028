0a4c05ee832107c658d2d5bed2e98aaf28cd612c
vasujoe2005 <vasujoe1985@gmail.com>
2026-07-02 13:01:11 +0530

    changes server port

--- notification-app-be/server.js
+++ notification-app-be/server.js
@@ -7,7 +7,10 @@ const priNotiRoutes=require('./routes/priNotiRoutes');
 app.use(express.json());
 app.use(cors());
 app.use("/",priNotiRoutes);
-const PORT=process.env.PORT||5000;
+const PORT=process.env.PORT||5002;
+app.get("/", (req, res) => {
+    res.send("Server Working");
+});
 app.listen(PORT,()=>{
     console.log(`Server active on port ${PORT}`);
 });
